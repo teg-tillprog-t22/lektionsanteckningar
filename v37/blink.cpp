65554989991e07ef576b6908ef6c2d80dfcf46c4
hemphen <michael.hemph@tabyenskilda.se>
2024-09-18 21:17:36 +0200

    Lägg till exempelkod för att blinka en LED

---
+++ v37/blink.cpp
@@ -0,0 +1,20 @@
+#include <arduino.h>
+
+const int LED_PIN = 4
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Startar!");
+
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    Serial.println("Tänder LED");
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+
+    Serial.println("Släcker LED");
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+}
